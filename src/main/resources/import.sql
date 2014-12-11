c6eb1a34f06ae498d4fd5397d067f13d913b80ee
Neven Cvetkovic <neven.cvetkovic@gmail.com>
2014-12-11 14:16:59 -0500

    Updated import.sql to reflect table names.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,12 +1,12 @@
-DELETE FROM hibernate_simple_user;
+DELETE FROM hibernate_named_queries_user;
 
-INSERT INTO hibernate_simple_user (name,email,created,active) VALUES ('John Doe','john@email.com','2001-01-01 00:00:00.000',true);
-INSERT INTO hibernate_simple_user (name,email,created,active) VALUES ('Jane Doe','jane@email.com','2002-01-01 00:00:00.000',true);
-INSERT INTO hibernate_simple_user (name,email,created,active) VALUES ('Jack Doe','jack@email.com','2003-01-01 00:00:00.000',false);
-INSERT INTO hibernate_simple_user (name,email,created,active) VALUES ('Jill Doe','jill@email.com','2004-01-01 00:00:00.000',false);
-INSERT INTO hibernate_simple_user (name,email,created,active) VALUES ('Jenn Doe','jenn@email.com','2005-01-01 00:00:00.000',true);
-INSERT INTO hibernate_simple_user (name,email,created,active) VALUES ('Jean Doe','jean@email.com','2010-01-01 00:00:00.000',false);
-INSERT INTO hibernate_simple_user (name,email,created,active) VALUES ('Joni Doe','joni@email.com','2012-01-01 00:00:00.000',true);
-INSERT INTO hibernate_simple_user (name,email,created,active) VALUES ('Jeff Doe','jeff@email.com','2014-01-01 00:00:00.000',false);
-INSERT INTO hibernate_simple_user (name,email,created,active) VALUES ('Jena Doe','jena@email.com','2000-01-01 00:00:00.000',true);
+INSERT INTO hibernate_named_queries_user (name,email,created,active) VALUES ('John Doe','john@email.com','2001-01-01 00:00:00.000',true);
+INSERT INTO hibernate_named_queries_user (name,email,created,active) VALUES ('Jane Doe','jane@email.com','2002-01-01 00:00:00.000',true);
+INSERT INTO hibernate_named_queries_user (name,email,created,active) VALUES ('Jack Doe','jack@email.com','2003-01-01 00:00:00.000',false);
+INSERT INTO hibernate_named_queries_user (name,email,created,active) VALUES ('Jill Doe','jill@email.com','2004-01-01 00:00:00.000',false);
+INSERT INTO hibernate_named_queries_user (name,email,created,active) VALUES ('Jenn Doe','jenn@email.com','2005-01-01 00:00:00.000',true);
+INSERT INTO hibernate_named_queries_user (name,email,created,active) VALUES ('Jean Doe','jean@email.com','2010-01-01 00:00:00.000',false);
+INSERT INTO hibernate_named_queries_user (name,email,created,active) VALUES ('Joni Doe','joni@email.com','2012-01-01 00:00:00.000',true);
+INSERT INTO hibernate_named_queries_user (name,email,created,active) VALUES ('Jeff Doe','jeff@email.com','2014-01-01 00:00:00.000',false);
+INSERT INTO hibernate_named_queries_user (name,email,created,active) VALUES ('Jena Doe','jena@email.com','2000-01-01 00:00:00.000',true);
 
